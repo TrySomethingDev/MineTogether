165eed117b41d423b1267a66e8d014a68270fc63
TrySomethingDev <trysomethingdev@gmail.com>
2024-01-20 11:25:21 -0500

    Initial upload

---
+++ src/main/java/net/trysomethingdev/tutorialmod/TutorialMod.java
@@ -0,0 +1,36 @@
+package net.trysomethingdev.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.trysomethingdev.tutorialmod.block.ModBlocks;
+import net.trysomethingdev.tutorialmod.item.ModItemGroups;
+import net.trysomethingdev.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmod");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+
+		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModblocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
+		FuelRegistry.INSTANCE.add(ModItems.METAL_DETECTOR, 10);
+		FuelRegistry.INSTANCE.add(ModBlocks.TRYSOMETHINGDEV_BLOCK, 999);
+
+	}
+}
---
+++ src/main/java/net/trysomethingdev/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.trysomethingdev.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
