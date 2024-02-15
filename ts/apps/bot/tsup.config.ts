0c802f2b4ad6f8abcf3c0167ad48d5b3bbd8f009
JoltCode <joe@joecc.dev>
2024-02-15 14:45:22 +0000

    fix(tsup): watch folders

--- ts/apps/bot/tsup.config.ts
+++ ts/apps/bot/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/**/*.ts"],
+  watch: ["src/**/*.ts"],
   skipNodeModulesBundle: true,
   tsconfig: "tsconfig.json",
   noExternal: ["@packages/db"],
